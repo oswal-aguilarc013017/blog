3fd3fef10f10b1ed0f14227eed577dffe458a5b1
oswal-aguilarc013017 <aguilarc731@gmail.com>
2017-10-13 15:33:02 -0600

    Borradores y eliminar

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,10 @@ from . import views
 urlpatterns = [
     url(r'^$', views.post_list),
     url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
-    url(r'^post/new/$', views.post_new, name='post_new'),
+    url(r'^post/nuevo/$', views.post_new, name='post_new'),
     url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
+    url(r'^borradores/$', views.post_draft_list, name='post_draft_list'),
+    url(r'^post/(?P<pk>\d+)/publicar/$', views.post_publish, name='post_publish'),
+    url(r'^post/(?P<pk>\d+)/eliminar/$', views.post_remove, name='post_remove'),
 
 ]
